9a9bb1d69852467116e949f80154eb8b8d29787b
llmgroup68 <138642474+llmgroup68@users.noreply.github.com>
2023-07-23 08:03:10 +0800

    Create tx.py

---
+++ tx.py
@@ -0,0 +1,13 @@
+#send a tx on Bitcoin testnet...
+import requests
+import re
+#url: Bitcoin testnet链接
+url='https://en.bitcoin.it/wiki/Testnet'
+#向它发送的tx信息
+tx={
+    'tx':'111222333'
+    }
+post_html=requests.post(url,data=tx)
+#输出返回的响应信息
+print(post_html.text)
+
